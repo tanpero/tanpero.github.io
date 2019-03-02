4b1d4d37fee1645226832a593fa0b1c1030d3d1d
Tanpero <3185750139@qq.com>
2019-03-02 12:03:03 +0800

    update a new way to show books and time

--- reservator-project/date.js
+++ reservator-project/date.js
@@ -1,11 +1,10 @@
-window.onload = function() {
+var showDate = function(span) {
 	
 	var date = new Date();
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
     var day = date.getDate();
     var nowDate = year + "年" + month + "月" + day + "日";
-    var span = document.getElementById("now");
 	
 	if (!span) {
 		return;
--- reservator-project/list.js
+++ reservator-project/list.js
@@ -1,18 +1,23 @@
-var showList function(list, table) {
-	
+var showList = function(list, table) {	
 	for (var i = 0, length = list.length; i < length; i += 1) {
 		var section = document.createElement("p");
 		var item = list[i];
 		var file = item + ".html";
 		var bookName = "《" + item + "》";
 		
-		p.innerHTML = "
-"<《" + item + "》](light/".html)
-
-> - [light](light/水经注.html)
-> - [newsprint](newsprint/水经注.html)
-> - [night](night/水经注.html)
-> - [pixyll](pixyll/水经注.html)
-> - [whitey](whitey/水经注.html)
-
----
+		section.innerHTML = 
+			"<p><a href=\"/reservator-project/light/" + file + "\">" + bookName + "</a></p>" +
+			"<blockquote>" +
+			"  <ul>" +
+			"    <li><a href=\"/reservator-project/light/" + "\">light</a></li>" +
+			"    <li><a href=\"/reservator-project/newsprint/" + file + "\">newsprint</a></li>" +
+			"    <li><a href=\"/reservator-project/night/" + file + "\">night</a></li>" +
+			"    <li><a href=\"/reservator-project/pixyll/" + file + "\">pixyll</a></li>" +
+			"    <li><a href=\"/reservator-project/whitey/" + file + "\">whitey</a></li>" +
+			"  </ul>" +
+			"</blockquote>" +
+			"<hr />";
+			
+		table.appendChild(section);
+	}
+}
